8180e54440eddf8097263b3a01b967638d5c2b6e
Duncan Marsh <slugbyte@slugbyte.com>
2022-01-02 00:31:06 +0800

    [feature] link the gitconfig

--- link.sh
+++ link.sh
@@ -6,6 +6,8 @@ mkdir -p ~/.config/openbox
 ln -sf $MOLD_ROOT/conf/rc.xml ~/.config/openbox/rc.xml
 ln -sf $MOLD_ROOT/conf/menu.xml ~/.config/openbox/menu.xml
 
+ln -sf $MOLD_ROOT/conf/.gitconfig ~/.gitconfig
+
 ln -sf $MOLD_ROOT/conf/zshrc ~/.zshrc
 
 ln -sf $MOLD_ROOT/conf/tmux.conf ~/.tmux.conf
